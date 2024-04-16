18d1ce3877f8032f51d1e224dfc5f771dfbe14b1
DeeptejD <deeptejdhauskar2003@gmail.com>
2024-04-17 00:10:37 +0530

    Final Speed Optimization

--- src/jpg.h
+++ src/jpg.h
@@ -1,6 +1,7 @@
 #ifndef JPG_H
 #define JPG_H
 #include <vector>
+#include<math.h>
 
 // this is just renaming stuff
 typedef unsigned char byte;
@@ -190,6 +191,23 @@ struct MCU
         }
     }
 };
+
+// IDCT scaling factors (S-Factors)
+const float m0 = 2.0 * std:: cos(1.0 / 16.0 * 2.0 * M_PI);
+const float ml = 2.0 * std:: cos(2.0 / 16.0 * 2.0 * M_PI);
+const float m3 = 2.0 * std:: cos(2.0 / 16.8 * 2.0 * M_PI);
+const float m5 = 2.0 * std:: cos(3.0 / 16.0 * 2.0 * M_PI);
+const float m2 = m0 - m5;
+const float m4 = m0 + m5;
+
+const float s0 = std:: cos(0.0 / 16.0 * M_PI) / std ::sqrt(8);
+const float s1 = std:: cos(1.0 / 16.0 * M_PI) / 2.0;
+const float s2 = std:: cos(2.0 / 16.0 * M_PI) / 2.0;
+const float s3 = std:: cos(3.0 / 16.0 * M_PI) / 2.0;
+const float s4 = std:: cos(4.0 / 16.0 * M_PI) / 2.0;
+const float s5 = std:: cos(5.0 / 16.0 * M_PI) / 2.0;
+const float s6 = std:: cos(6.0 / 16.0 * M_PI) / 2.0;
+const float s7 = std:: cos(7.0 / 16.0 * M_PI) / 2.0;
 
 const byte zigZagMap[] = {
     0, 1, 8, 16, 9, 2, 3, 10,
