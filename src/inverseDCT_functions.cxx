d6eb0ba314871a66d1b07a58dbc43b9765b6bc7c
DeeptejD <deeptejdhauskar2003@gmail.com>
2024-04-16 23:28:06 +0530

    Optimization: Precomputation of IDCT

--- src/inverseDCT_functions.cxx
+++ src/inverseDCT_functions.cxx
@@ -7,32 +7,26 @@
 void inverseDCT(const Header *const header, MCU *const mcus);
 
 // inverse DCT on each mcu
-void inverseDCTComponent(int *const component);
+void inverseDCTComponent(const float *const idctMap, int *const component);
 
-void inverseDCTComponent(int *const component)
+void inverseDCTComponent(const float *const idctMap, int *const component)
 {
     int result[64] = {0};
     for (uint y = 0; y < 8; y++)
     {
         for (uint x = 0; x < 8; x++)
         {
-            double sum = 0.0;
-            for (uint i = 0; i < 8; i++)
+            float sum = 0.0;
+            // i was changed to v and j was changed to u to stick with the convention
+            for (uint v = 0; v < 8; v++)
             {
-                for (uint j = 0; j < 8; j++)
+                for (uint u = 0; u < 8; u++)
                 {
-                    double ci = 1.0;
-                    double cj = 1.0;
-                    if (i == 0)
-                        ci = 1.0 / std::sqrt(2.0);
-                    if (j == 0)
-                        cj = 1.0 / std::sqrt(2.0);
-                    sum += ci * cj * component[i * 8 + j] *
-                           std::cos((2.0 * x + 1.0) * j * M_PI / 16.0) *
-                           std::cos((2.0 * y + 1.0) * i * M_PI / 16.0);
+                    sum += component[v * 8 + u] *
+                           idctMap[u * 8 + x] *
+                           idctMap[v * 8 + y];
                 }
             }
-            sum /= 4.0;
             result[y * 8 + x] = (int)sum;
         }
     }
@@ -43,10 +37,21 @@ void inverseDCTComponent(int *const component)
 
 void inverseDCT(const Header *const header, MCU *const mcus)
 {
+    // prepare idctMap
+    float idctMap[64];
+    for (uint u = 0; u < 8; u++)
+    {
+        const float c = (u == 0) ? (1.0 / std::sqrt(2.0) / 2.0) : (1.0 / 2.0); // division by 2 is added to avoid the division of 4 we were doing with the sum in the previous function
+        for (uint x = 0; x < 8; x++)
+        {
+            idctMap[u * 8 + x] = c * std::cos((2.0 * x + 1.0) * u * M_PI / 16.0);
+        }
+    }
+
     const uint mcuHeight = (header->height + 7) / 8;
     const uint mcuWidth = (header->width + 7) / 8;
 
     for (uint i = 0; i < mcuHeight * mcuWidth; i++)
         for (uint j = 0; j < header->numComponents; j++)
-            inverseDCTComponent(mcus[i][j]);
+            inverseDCTComponent(idctMap, mcus[i][j]);
 }
